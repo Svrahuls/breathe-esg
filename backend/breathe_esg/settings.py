600f8e26351f870789d84d348202abd13dffc9fc
ayush <mailavinashsingh95@gmail.com>
2026-05-28 23:53:44 +0530

    fix cors for netlify

--- backend/breathe_esg/settings.py
+++ backend/breathe_esg/settings.py
@@ -123,18 +123,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Django REST Framework
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-    ],
+    
 
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
 
-    "DEFAULT_RENDERER_CLASSES": [
-        "rest_framework.renderers.JSONRenderer",
-    ],
+    
 }
 
 # CORS
